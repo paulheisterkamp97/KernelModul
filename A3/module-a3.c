19d5af7b90f4a8174c62a805dd050d96e9ff7215
Paul Heisterkamp <paul297h@gmail.com>
2020-11-21 16:18:23 +0100

    a3

--- A3/module-a3.c
+++ A3/module-a3.c
@@ -1,16 +1,4 @@
-#include <linux/init.h>
-#include <linux/module.h>
-#include <linux/kernel.h>
-
-static int __init test_init(void){
-	printk(KERN_INFO "Modul wurde geladen\n");
+int main(void){
+	printf("Hello World\n");
 	return 0;
 }
-
-
-static void __exit  test_exit(void){
-	printk(KERN_INFO "Modul wurde rausgeschmissen\n");
-}
-
-module_init(test_init);
-module_exit(test_exit);
