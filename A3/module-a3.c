eeabf2c85e16889bf5519a06dce52acf7b24cf6b
Paul Heisterkamp <paul297h@gmail.com>
2020-11-21 16:39:34 +0100

    3,8

--- A3/module-a3.c
+++ A3/module-a3.c
@@ -2,6 +2,6 @@
 #include <sys/types.h> 
 #include <unistd.h> 
 int main(int argc, char *argv[]){
-	printf("Hello World %s\n", (int)argv[1]);
+	printf("Hello World %d\n", (int)argv[1]);
 	return 0;
 }
