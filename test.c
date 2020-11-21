feb257752e9317402f10c3dde7a030a191f5eb95
Paul Heisterkamp <paul297h@gmail.com>
2020-11-21 15:35:33 +0100

    Hello world

---
+++ test.c
@@ -0,0 +1,16 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+static int __init test_init(void){
+	printk(KERN_INFO "Modul wurde geladen\n");
+	return 0;
+}
+
+
+static int __exit  test_exit(void){
+	printk(KERN_INFO "Modul wurde rausgeschmissen\n");
+}
+
+module_init(test_init);
+module_exit(test_exit);
