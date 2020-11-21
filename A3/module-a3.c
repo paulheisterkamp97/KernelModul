4c82d2a96241751bdbb3666d3400c098332bde40
Paul Heisterkamp <paul297h@gmail.com>
2020-11-21 16:33:26 +0100

    3.4

--- A3/module-a3.c
+++ A3/module-a3.c
@@ -1,6 +1,7 @@
-#include <stdio.h>
-int main(void){
-	fork();
-	printf("Hello World\n");
+#include <stdio.h> 
+#include <sys/types.h> 
+#include <unistd.h> 
+int main(int argc, char *argv[]){
+	printf("Hello World %d\n", argv[0]);
 	return 0;
 }
