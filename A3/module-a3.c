a1f419a37be089f72861e92c63ebf520ab7bde4c
Paul Heisterkamp <s4paheis@uni-trier.de>
2020-11-23 10:44:01 +0000

    Fertiges Programm A3

--- A3/module-a3.c
+++ A3/module-a3.c
@@ -2,18 +2,23 @@
 #include <sys/types.h> 
 #include <unistd.h> 
 #include <sys/time.h>
-#include <float.h>
+#include <limits.h>
 
 
 int main(int argc, char *argv[]){
-	
-	int maxR = 100; // Anzahl Durchläufe
+	int token[4];//anzahl,min,max,avg
+	token[0] = 0;
+	token[1] = INT_MAX;
+	token[2] = 0;
+	token[3] = 0;
+
+
+	int maxR = 10; // Anzahl Durchläufe
 	int subpr = 100; // Anzahl subprozesse
 
-	int token = 0;
-	int procNr = 0;
-	int fd[subpr+1][2];
-	int id = 1;
+	int procNr = 0; // ID des Prozesses
+	int fd[subpr+1][2];//Filedescriptor 
+	int id = 1; // Identifier für parent und child prozesse
 
 	for (int i = 0; i < subpr+1; ++i)
 	{
@@ -33,40 +38,39 @@ int main(int argc, char *argv[]){
 	}
 
 	if(id != 0){//Parent
-		procNr = procNr + 1;
-		double Tmin = DBL_MAX;
-		double Tcum = 0;
-		double Tmax = 0;
-
+		procNr = procNr + 1; //Parent bekommt den letzten Platz in der Przess liste
 		struct timeval start, stop;
 
-		write(fd[0][1],&token,sizeof(int));
+		write(fd[0][1],token,sizeof(int)*4); // Token Umlauf wird gestartet
 		gettimeofday(&start,NULL);
 		
 
-		while(token < maxR){
-			read(fd[procNr][0],&token, sizeof(int));
+		while(token[0] < maxR){
+			read(fd[procNr][0], token, sizeof(int)*4);
 			gettimeofday(&stop,NULL);
-			token += 1;
-
-			double Tcurr = (double)(stop.tv_usec - start.tv_usec) / 1000;
-			if(Tcurr<Tmin)Tmin = Tcurr;
-			if(Tcurr>Tmax)Tmax = Tcurr;
-			Tcum += Tcurr;
 			
-			if(token % 10 == 0){
-				printf("umlauf %d in %f microsekunden\n",token,Tcurr);
-				printf("Durschnitt: %f // Minimal: %f // Maximal: %f \n",Tcum/token,Tmin,Tmax);
+			//Zeit nehmen und Token werte aktualisieren
+			int time = (int)(stop.tv_usec - start.tv_usec);
+			if(token[1]>time)token[1]=time;
+			if(token[2]<time)token[2]=time;
+			token[3] += time;
+			token[0]++;
+			
+			if(token[0] % 1 == 0){//Ausgabe des Tokens
+				printf("Umlauf %d in %d microsekunden\n",token[0],time);
+				printf("Durschnitt: %d // Minimal: %d // Maximal: %d\n",token[3]/token[0],token[1],token[2]);
+				printf("--------------------------------------------------------\n");
 			}
-			write(fd[0][1],&token,sizeof(int));
 			gettimeofday(&start,NULL);
+			write(fd[0][1],token,sizeof(int)*4);			
 		}
 
 	}
-	if(id == 0 ){//child		
-		while(token < maxR){
-			read(fd[procNr][0],&token, sizeof(int));
-			write(fd[procNr+1][1],&token,sizeof(int));
+	if(id == 0 ){//child
+		while(token[0] < maxR){
+			
+			read(fd[procNr][0],token, sizeof(int)*4);
+			write(fd[procNr+1][1],token,sizeof(int)*4);
 		}
 
 	}
